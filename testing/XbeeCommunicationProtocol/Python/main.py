1281f3cc2bf303212b57ba8e4dc1b92df0a30cc5
Matthijs Visser <matthijs.visser@hva.nl>
2016-01-13 14:28:27 +0100

    servopositions toegevoegd

--- testing/XbeeCommunicationProtocol/Python/main.py
+++ testing/XbeeCommunicationProtocol/Python/main.py
@@ -176,6 +176,20 @@ def GetAllAngles():
 
     return allAngles
 
+def setServoPositions(XBee, servoNum, servoData):
+    data = list()
+    if servoNum < 0 or servoNum > 18:
+        return 0
+    data.append(servoNum)
+    for i in range(0, servoNum):
+        servoId = servoData[i*2]
+        servoPosition = servoData[i*2 + 1]
+        if(position < 0 or position > 1023):
+            return 0
+        data.append(servoId)
+        data.append(servoPosition)
+    XBee.sendPacket(1, bytearray(data))
+
 
 vrep.simxFinish(-1)
 # clientID = vrep.simxStart('192.168.10.151',19999,True,True,5000,5) # Connect to V-REP
